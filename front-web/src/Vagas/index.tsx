48053e1af2ca2a136ffce5a242317b0f6bfa04d4
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-17 20:24:35 -0300

    adding page vagas

--- front-web/src/Vagas/index.tsx
+++ front-web/src/Vagas/index.tsx
@@ -1,9 +1,15 @@
+import Header from '../Header';
+import {ReactComponent as Imageexclamacao} from './image-exclamacao.svg';
 import './style.css';
 function Login() {
     return (    
-        <div>
-            <p>Vagas</p>
+        <>
+        <Header />
+        <div className="vagas-content">
+            <Imageexclamacao  className="image-exclamacao"/>
+            <p className="vagas-title">Encontre a Vaga Ideal</p>
         </div>
+        </>
     );
 }
 
