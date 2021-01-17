2c08bd546405a647fa56dc8c16cbdf614958130d
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-01-17 20:29:07 -0300

    adding page vagas

--- front-web/src/Login/index.tsx
+++ front-web/src/Login/index.tsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Fundo } from './fundo-image.svg';
 import './style.css';
 function Login() {
-    return (    
+    return (
         <>
-        <Fundo className= "image-fundo-login"/>
-        <div className="login-content">
+            <Fundo className="image-fundo-login" />
+            <div className="login-content">
                 <h1 className="login-content-title">Bem vindo de volta!</h1>
                 <form className="form-login">
-                    <input className="input-login" value="Login"/>
-                    <input className="input-login" value="Senha"/>
+                    <input className="input-login" value="Login" />
+                    <input className="input-login" value="Senha" />
                     <Link to="/vagas" className="login-form-button">Próximo</Link>
                 </form>
-                
+
             </div>
         </>
     );
--- front-web/src/Vagas/index.tsx
+++ front-web/src/Vagas/index.tsx
@@ -1,14 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
-import {ReactComponent as Imageexclamacao} from './image-exclamacao.svg';
+import { ReactComponent as Imageexclamacao } from './image-exclamacao.svg';
 import './style.css';
 function Login() {
-    return (    
+    return (
         <>
-        <Header />
-        <div className="vagas-content">
-            <Imageexclamacao  className="image-exclamacao"/>
-            <p className="vagas-title">Encontre a Vaga Ideal</p>
-        </div>
+            <Header />
+            <div className="vagas-content">
+                <Imageexclamacao className="image-exclamacao" />
+                <p className="vagas-title">Encontre a Vaga Ideal</p>
+
+                <form className="form-pesquisa">
+                    <input className="input-pesquisa" value="Login" />
+                    <input className="input-pesquisa" value="Senha" />
+                    <input className="input-pesquisa" value="Digite uma cidade" />
+                    <Link to="/" className="pesquisa-form-button">pesquisar</Link>
+                </form>
+            </div>
         </>
     );
 }
